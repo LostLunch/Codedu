1423755cd5ef9974ada3825ac4958957555f29bd
LostLunch <songage2008@gmail.com>
2025-12-19 15:12:53 +0900

    1차

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit as st
 
+st.title("CodEdu 코딩 독학의 시작")
+st.header("로그인")
+username = st.text_input("아이디")
+password = st.text_input("비밀번호", type="password")
+level = st.selectbox("수준", ["초급", "중급", "고급"])
